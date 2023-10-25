63a469fcb32ce61452832babd7d541ddc8172e1a
Hector Vera Martinez <111088161+HVGamerYT7@users.noreply.github.com>
2023-10-24 22:13:59 -0300

    Especificacion de procesos con comentarios

--- src/modelos/administrador.jsx
+++ src/modelos/administrador.jsx
@@ -2,7 +2,8 @@ class Administrador{
     nombre = '';
     contrasenia = '';
 
-    verificarDocumentos(){ //Por que tiene este metodo?
-
+    verificarDocumentos(documentopdf){ //Por que tiene este metodo?
+        //metodo recive un documento y lo guarda en la base de datos , este tiene que ser en formato pdf.
+        // luego el administrador tiene que verificas este documento y validar que es veridico a mano
     }
 }
--- src/modelos/usuario.jsx
+++ src/modelos/usuario.jsx
@@ -3,19 +3,25 @@ class Usuario{
     rut = '';
     correo = '';
     contrasena = '';
-    puntos = ''; //A que se refiere estos puntos?
+    puntos = ''; //A que se refiere estos puntos? //puntos acumulados al responder los juegos aperecidos en pantalla solo las que tiene correctas
+    // y podran usarse como canje de descuento
     dirreccion = '';
 
-    cambiarNombre(){ //Cambiar el nombre del usuario
-
+    cambiarNombre(string nombreNuevo){ //Cambiar el nombre del usuario
+        //al usuario se le preguntara si esta seguro de cambiar el nombre
+        //en el caso sea correcto se cambiara
+        //en el caso contrario se mantendra igual
     }
 
-    cambiarContrasenia(){ //Cambiar contraseña, no recibe nada?
-
+    cambiarContrasenia(string contraseniaActual, string nuevaContrasenia, string nuevaContraseniaValidacion){ //Cambiar contraseña, no recibe nada?
+        //resive contraseña actual para validar con la que ya tiene
+        // en el caso correcto se cambiara la nueva contraseña con su validacion
+        //si ambas son iguales se realizara el cambio correspondiente 
+        //en el caso contrario se mandar un mensaje se "error en la solicitud" y maximo de 3 intentos
     }
 
-    cambiarDireccion(){ //Cambiar dirreccion del usuario, no recibe nada?
-
+    cambiarDireccion(string direccionNueva){ //Cambiar dirreccion del usuario, no recibe nada?
+        //resivira la nueva direccion y la cambiara
     }
 
 }
