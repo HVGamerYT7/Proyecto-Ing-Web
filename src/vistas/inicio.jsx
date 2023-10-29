a9c5116f2cc8bca4a0b969642e120305f7b303a7
JonathanVegafz <jonathan.vega.f@mail.pucv.cl>
2023-10-29 01:13:09 -0300

    Tratar de centrar un div

--- src/vistas/inicio.jsx
+++ src/vistas/inicio.jsx
@@ -22,12 +22,29 @@ function Inicio() {
                         <h3>Cuidar de la Tierra, cuidar de ti: Productos reciclados para un futuro mejor.</h3>
                     </section>
                 </div>
-
-                
-
-                
             </article>
             
+            <section className='seccionCategoria'>
+                <div className='contenedorCategorias'>
+                    <article className='cardCategorias'>
+                        <img className='imgCategorias' src={'src/assets/camisa.png'}></img>
+                        <h3>Categoria</h3>
+                    </article>
+                    <article className='cardCategorias'>
+                        <img className='imgCategorias' src={'src/assets/camisa.png'}></img>
+                        <h3>Categoria</h3>
+                    </article>
+                    <article className='cardCategorias'>
+                        <img className='imgCategorias' src={'src/assets/camisa.png'}></img>
+                        <h3>Categoria</h3>
+                    </article>
+                    <article className='cardCategorias'>
+                        <img className='imgCategorias' src={'src/assets/camisa.png'}></img>
+                        <h3>Categoria</h3>
+                    </article>
+                </div>
+            </section>
+
                     
         </div>
        
