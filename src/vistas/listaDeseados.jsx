e3c5b5a934a698487681c9dd6ea1a656b69ebfc0
JonathanVegafz <jonathan.vega.f@mail.pucv.cl>
2023-10-29 18:01:55 -0300

    Lista de deseados

--- src/vistas/listaDeseados.jsx
+++ src/vistas/listaDeseados.jsx
@@ -1,3 +1,6 @@
+import { Button } from 'reactstrap';
+import './estilos/listaDeseados.scss'
+
 function ListaDeseados() {
 
     const limpiarListaDeseados = ()=>{
@@ -12,7 +15,59 @@ function ListaDeseados() {
         
     }
 
-    return (<><h1>Lista de Deseados</h1></>);
+    return (<>
+        <div className='contenedorPadreDeseados'>
+            <div className="containerDeseados">
+                <h2>Lista Deseados</h2> 
+                <article className='contenedorProductoDeseado'>
+                    <img className='imgProductoDeseado' src='src/Img de productos/cepillo de dientes ecologicos.jpg' alt='Cepillo'></img>    
+                    <div className='contenedorDatosProductoDeseado'>
+                        <h3>Nombre</h3>
+                        <h3>Precio</h3>
+                        <div className='contenedorBotonesProductos'>
+                            <Button color='primary'>Comprar</Button>
+                            <Button color='primary'>Ir a ver</Button>
+                            <Button color='danger'>Eliminar</Button>
+                        </div>
+
+                    </div>
+                </article>
+
+                <article className='contenedorProductoDeseado'>
+                    <img className='imgProductoDeseado' src='src/Img de productos/cepillo de dientes ecologicos.jpg' alt='Cepillo'></img>    
+                    <div className='contenedorDatosProductoDeseado'>
+                        <h3>Nombre</h3>
+                        <h3>Precio</h3>
+                        <div className='contenedorBotonesProductos'>
+                            <Button color='primary'>Comprar</Button>
+                            <Button color='primary'>Ir a ver</Button>
+                            <Button color='danger'>Eliminar</Button>
+                        </div>
+
+                    </div>
+                </article>
+
+                <article className='contenedorProductoDeseado'>
+                    <img className='imgProductoDeseado' src='src/Img de productos/cepillo de dientes ecologicos.jpg' alt='Cepillo'></img>    
+                    <div className='contenedorDatosProductoDeseado'>
+                        <h3>Nombre</h3>
+                        <h3>Precio</h3>
+                        <div className='contenedorBotonesProductos'>
+                            <Button color='primary'>Comprar</Button>
+                            <Button color='primary'>Ir a ver</Button>
+                            <Button color='danger'>Eliminar</Button>
+                        </div>
+
+                       
+
+                    </div>
+                </article>
+
+
+            </div>
+        </div>
+        
+    </>);
 }
 
 export default ListaDeseados;
