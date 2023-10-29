07743e5d4cf92ff7cc0bf09df5e8802fbae11803
JonathanVegafz <jonathan.vega.f@mail.pucv.cl>
2023-10-29 03:01:50 -0300

    Login

--- src/vistas/inisiosesion.jsx
+++ src/vistas/inisiosesion.jsx
@@ -1,8 +1,21 @@
-
+import './estilos/login.scss'
 
 export const InicioSesion = () =>{
     return (
-        <><h1>InicioSesion</h1></>
+        <div className="login">
+
+            <article className='cardLogin'>
+                <h3>Inicio Sesion</h3>
+
+                
+                <input></input>
+                <input></input>
+                
+
+            </article>
+
+
+        </div>
     );
 }
 
