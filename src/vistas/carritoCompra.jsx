f8b60ad0e1e331854ee1b001939cbc59d60dfff6
JonathanVegafz <jonathan.vega.f@mail.pucv.cl>
2023-10-29 18:17:01 -0300

    Carrito de compra

--- src/vistas/carritoCompra.jsx
+++ src/vistas/carritoCompra.jsx
@@ -1,3 +1,6 @@
+import { Button } from 'reactstrap';
+import './estilos/carritoCompra.scss';
+
 function CarritoCompra() {
 
     const limpiarCarrito = ()=>{
@@ -20,7 +23,33 @@ function CarritoCompra() {
 
     }
     
-    return ( <><h1>Carrito</h1></> );
+    return ( <>
+        <div className='contenedorPadreCarrito'>
+            <div className="containerCarrito">
+                <h2>Carrito de compra</h2> 
+                <article className='contenedorProductoCarrito'>
+                    <img className='imgProductoCarrito' src='src/Img de productos/cepillo de dientes ecologicos.jpg' alt='Cepillo'></img>    
+                    <div className='contenedorDatosProductoCarrito'>
+                        <h3>Nombre</h3>
+                        <h3>Precio</h3>
+                        <div className='contenedorBotonesProductosCarrito'>
+                            <Button color='primary'>Ir a ver</Button>
+                            <Button color='danger'>Eliminar</Button>
+                        </div>
+
+                    </div>
+                </article>
+
+                <article className='contenedorProductoCarrito'>
+                    <h3 style={{marginRight: '40px'}}>Total:1000 </h3>
+                    <Button color='success'>Comprar productos</Button>
+                </article>
+                
+
+            </div>
+        </div>
+    
+    </> );
 }
 
 export default CarritoCompra;
